6cbf0026623e60efd5b4fd162f9e4f00a6034f97
James Howell <james.howell@aia.co.uk>
2018-04-19 16:04:05 +0100

    final code tweaks

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -54,9 +54,24 @@ exports.createStore = async(req, res) => {
 };
 
 exports.getStores = async(req, res) => {
+  const page = req.params.page || 1;
+  const limit = 4;
+  const skip = (page * limit) - limit;
+  
   //1. Query the database for a list of all stores
-  const stores = await Store.find();
-  res.render('stores', { title: 'Stores', stores});
+  const storesPromise = Store
+    .find()
+    .skip(skip)
+    .limit(limit)
+    .sort({ created: 'desc' })
+
+  const countPromise = Store.count();
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+
+  const pages = Math.ceil(count / limit);
+
+  res.render('stores', { title: 'Stores', stores, page, pages, count});
 };
 
 exports.getStoreBySlug = async(req, res, next) => {
@@ -155,4 +170,9 @@ exports.getHearts = async(req, res) => {
     _id: { $in: req.user.hearts}
   });
   res.render('stores', { title: 'Hearted Stores', stores});
+};
+
+exports.getTopStores = async (req, res) => {
+  const stores = await Store.getTopStores();
+  res.render('topStores', { title: 'Top Stores' ,stores });
 };
